37d83b40472793c31a5f8525a19a74a037540353
vquennessen <viquennessen@gmail.com>
2025-04-04 17:54:59 -0700

    better update

--- R/clutches_to_sample.R
+++ R/clutches_to_sample.R
@@ -349,12 +349,12 @@ clutches_to_sample <- function(n_sims = 10000,
 
     }
 
-            # print progress while running
-          update1 <- paste(Sys.time(), ' - ', scenario, ' - sample size ',
-                          sample_size, ' - ', paternal_contribution_mode, ' - ',
-                          n_sims, ' sims - OSR ', OSRs[osr], ' done!', sep = '')
+    # print progress while running
+    update1 <- paste(Sys.time(), ' - ', scenario, ' - sample size ',
+                     sample_size, ' - ', paternal_contribution_mode, ' - ',
+                     n_sims, ' sims - OSR ', OSRs[osr], ' done!', sep = '')
 
-          write(update1, file = 'progress.txt', append = TRUE)
+    write(update1, file = 'progress.txt', append = TRUE)
 
   }
 
