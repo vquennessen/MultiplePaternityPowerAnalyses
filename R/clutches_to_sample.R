19faebe349856b29459b8dd8a24c15ab3f9f4fed
vquennessen <viquennessen@gmail.com>
2025-04-04 18:33:09 -0700

    put the update in the right spot

--- R/clutches_to_sample.R
+++ R/clutches_to_sample.R
@@ -348,8 +348,6 @@ clutches_to_sample <- function(n_sims = 10000,
       # add ID to dataframe
       DF2$Proportion[index] <- all_fathers_ID
 
-    }
-
     # print progress while running
     update1 <- paste(lubridate::now(), ' - ', scenario, ' - sample size ',
                      sample_size, ' - ', paternal_contribution_mode, ' - ',
@@ -357,6 +355,8 @@ clutches_to_sample <- function(n_sims = 10000,
                      prop, ' done!', sep = '')
 
     write(update1, file = 'progress.txt', append = TRUE)
+
+    }
 
   }
 
